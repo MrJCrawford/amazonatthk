faa6158342d3e5283f763eee5fa3b76d390b1b59
MrJCrawford <azwin.lam@gmail.com>
2020-09-15 00:30:30 +0800

    Test

--- frontend/src/screens/PlaceOrderScreen.js
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -89,18 +89,17 @@ const ServiceChargeIcon = () => (
             Payment Method: {cart.payment.paymentMethod}
             </li>
             <ol className="payment-methods">
-
-            <li>
-            For HSBC Payme: <a href=" https://payme.hsbc/60261084" target="_blank" rel="noopener noreferrer"> Tap to PayMe!</a>
-            </li>
-            <img className="payme" src="\images\hsbcpayme.jpg" alt="payme"></img>
-            <li>
-            For FPS: Payment can be sent to 60261084.
-            </li>
-            <li>
-            For Bank Transfer:
-            <p>HSBC bank account 405 222 043 292 (Chan T** W**g)</p>
-            </li>
+              <li>
+              For HSBC Payme: <a href=" https://payme.hsbc/60261084" target="_blank" rel="noopener noreferrer"> Tap to PayMe!</a>
+              </li>
+              <img className="payme" src="\images\hsbcpayme.jpg" alt="payme"></img>
+              <li>
+              For FPS: Payment can be sent to 60261084.
+              </li>
+              <li>
+              For Bank Transfer:
+              <p>HSBC bank account 405 222 043 292 (Chan T** W**g)</p>
+              </li>
             </ol>
 
 
